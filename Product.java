1fa7412752925739cb1e2bc95116a4dc08b0c780
marcin7777 <marcinmz9@tlen.pl>
2020-02-06 23:10:01 +0100

    First change in Product class that is about price in  class constructor

---
+++ Product.java
@@ -0,0 +1,37 @@
+public abstract class Product {
+	private final String name;
+
+	private final BigDecimal price;
+
+	private final BigDecimal taxPercent;
+
+	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null || name.equals("")) {
+			throw new IllegalArgumentException("Product name cannot be null");
+		} 
+		if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException("Product cannot be null");
+		}
+		this.name = name;
+		this.price = price;
+		this.taxPercent = tax;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public BigDecimal getTaxPercent() {
+		return taxPercent;
+	}
+
+	public BigDecimal getPriceWithTax() {
+		return this.price.add(this.taxPercent.multiply(this.price));
+	}
+	
+}
+

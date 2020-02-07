b438387c4944daf51d2552017912f22e1fc5a805
marcin7777 <marcinmz9@tlen.pl>
2020-02-07 18:07:14 +0100

    Change code in line 9, created HashMap

---
+++ Invoice.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.mwo.invoice;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.edu.agh.mwo.invoice.product.Product;
+
+public class Invoice {
+	private Map<Product,Integer> products = new HashMap<>();
+
+	public void addProduct(Product product) {
+		// TODO: implement
+	}
+
+	public void addProduct(Product product, Integer quantity) {
+		// TODO: implement
+	}
+
+	public BigDecimal getSubtotal() {
+		return null;
+	}
+
+	public BigDecimal getTax() {
+		return null;
+	}
+
+	public BigDecimal getTotal() {
+		return null;
+	}
+}
+

d83f42beedbbf2d94080db2a9009449b3a2a6e7d
marcin7777 <marcinmz9@tlen.pl>
2020-02-07 21:36:00 +0100

    Entire class Invoice once again

--- Invoice.java
+++ Invoice.java
@@ -1,12 +1,10 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
-
 
 public class Invoice {
 	private Collection<Product> products;
@@ -16,67 +14,66 @@ public class Invoice {
 
 	
 	Invoice() {
-		netValue = new BigDecimal("0");
+		subTotal = new BigDecimal("0");
 		total = new BigDecimal("0");
 		tax = new BigDecimal("0");
 		products = new ArrayList<Product>();
 
 	}
 
-
-        public void addProduct(Product product) {
-		products.add(Product);
+	public void addProduct(Product product) {
+		products.add(product);
 		update();
 	}
 
 	public void addProduct(Product product, Integer quantity) {
 		if (quantity <= 0) {
-                     throw new IllegalArgumentException();
+			throw new IllegalArgumentException();
 		}
-		for (int i =0; i < quantity; i++) {
-		products.add(product);
-		} 
+		for (int i = 0; i < quantity; i++) {
+			products.add(product);
+		}
 		update();
 	}
-        public void setSubTotal() {
-
-        subTotal = BigDecimal.ZERO;
-	for (Product product: products) {
-		subTotal = subTotal.add(product.getPrice());
-		}
-	}
-
 	public BigDecimal getSubtotal() {
 		return subTotal;
 	}
+	public void setSubtotal() {
+		subTotal = BigDecimal.ZERO;
+		for (Product product : products) {
+			subTotal = subTotal.add(product.getPrice());
+		}
+	}
 
-	public void setTax() {
-         tax =  total.subtract(subTotal);
-
- 	}
 	public BigDecimal getTax() {
 		return tax;
+	}
+	
+	public void setTax() {
+		tax = total.subtract(subTotal);
 	}
 
 	public BigDecimal getTotal() {
 		return total;
-	}	
-
+	}
 	public void setTotal() {
-
-	total = BigDecimal.ZERO;
-	for (Product product: products) {
-	total = total.add(product.getPriceWithTax());
+		total = BigDecimal.ZERO;
+		for (Product product : products) {
+			total = total.add(product.getPriceWithTax());
 		}
 	}
+
 	public static Invoice create() {
-	return new Invoice();
-	}	
-        
-        public void update() {
-	setSubtotal();
-	setTotal();
-	setTax();
+		return new Invoice();
 	}
+
+	public void update() {
+		setSubtotal();
+		setTotal();
+		setTax();
+	}
+
+	
+
 }
 
